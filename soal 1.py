1860cd2f4096a3dbbee184bae8525922362a68b3
fathurrahman2304 <113899829+fathurrahman2304@users.noreply.github.com>
2022-09-20 14:19:37 +0700

    Add files via upload

---
+++ soal 1.py
@@ -0,0 +1,12 @@
+name = "Fathur Rahman"
+address = "Swasembada Barat VI no.1,North Jakarta"
+umur = 18
+angkatan = 2022
+is_male = True
+if (is_male == True):
+ gender = "Laki-laki"
+else:
+ gender = "perempuan"
+ 
+print("nama saya", name + ",saya tinggal di " + address,", Umur saya sekitar "
+ + str(umur) + " angkatan ", angkatan," saya adalah " + gender)
