04ca5bcc25a434a30b5692e503bfefacb9d09b2b
fathurrahman2304 <113899829+fathurrahman2304@users.noreply.github.com>
2022-09-20 20:44:04 +0700

    Update Soal 2.py

--- Soal 2.py
+++ Soal 2.py
@@ -7,11 +7,11 @@ Panjang = float(input("Masukkan angka: "))
 Lebar = float(input("Masukkan angka: "))
 Satuan = str(input("masukkan satuan: "))
 Luas = (Panjang * Lebar)
-print("Luas persegi panjang: ", Luas , Satuan)
+print("Luas persegi panjang = ",Panjang, "*", Lebar ,"=", Luas , Satuan)
 
 # Luas segitiga
 print("----LUAS SEGITIGA----")
 Alas = float(input("Masukkan angka: "))
 Tinggi = float(input("Masukkan angka: "))
 Luassegitiga = (Alas * Tinggi / 2)
-print("Luas persegi panjang: ", Luassegitiga  , Satuan)
+print("Luas persegi panjang = ",Alas,"*",Tinggi,"/2" ,"=",Luassegitiga  , Satuan)
